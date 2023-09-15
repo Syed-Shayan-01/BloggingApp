23281090c9481158e28d9156ff4315b52275c5ce
Syed Shayan <shayansyed712@gmail.com>
2023-09-15 20:16:37 +0500

    contact page

--- src/pages/allPages/contactUs/contactUs.js
+++ src/pages/allPages/contactUs/contactUs.js
@@ -8,7 +8,7 @@ const contactUs = () => {
                     <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
                     <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
                     <form action="#" class="space-y-8">
-                        <div> 
+                        <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
                             <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 focus:outline-blue-500 dark:bg-gray-700 dark:border-gray-600  dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
                         </div>
