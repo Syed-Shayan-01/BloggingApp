c7af82bc81bb87b5ec9e4791c7805c3082d45cc6
Syed Shayan <shayansyed712@gmail.com>
2023-10-05 23:26:04 +0500

    new changes in the blog

--- src/pages/Forms/login.js
+++ src/pages/Forms/login.js
@@ -1,11 +1,16 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Forms from "@/components/auth/Forms"
 import { getSession, signIn } from 'next-auth/react'
+import { useRouter } from "next/router"
 const login = () => {
-    const onSubmit = async (email, password) => {
-        const data = await signIn('credentials', { redirect: false, email, password });
-        console.log(data)
+  const router = useRouter();
+  const onSubmit = async (email, password) => {
+    const data = await signIn('credentials', { redirect: false, email, password });
+    if (data.ok) {
+      router.replace('/userDashboard/Dashboard')
     }
-    return (<Forms signin={true} onFormSubmit={onSubmit} />)
+  }
+  return (<Forms signin={true} onFormSubmit={onSubmit} />)
 }
 
 export default login;
@@ -13,19 +18,19 @@ export default login;
 
 
 export const getServerSideProps = async ({ req }) => {
-    const session = await getSession({ req });
-    if (session) {
-      return {
-        redirect: {
-          destination: "/allPages/profile",
-          parmanent: false,
-        },
-      };
-    }
-  
+  const session = await getSession({ req });
+  if (session) {
     return {
-      props: {
-        session,
+      redirect: {
+        destination: "/allPages/profile",
+        parmanent: false,
       },
     };
+  }
+
+  return {
+    props: {
+      session,
+    },
   };
+};
--- src/pages/Forms/signup.js
+++ src/pages/Forms/signup.js
@@ -1,28 +1,33 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Forms from "@/components/auth/Forms"
-
+import { useRouter } from "next/router"
 const signup = () => {
-    const onSubmit =  async ( email, password ) => {
-        const data = { email, password }
-        try {
-            const response = await fetch("http://localhost:3000/api/auth/user", {
-              method: "POST", // or 'PUT'
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(data),
-            });
-        
+  const router = useRouter();
+  const onSubmit = async (email, password) => {
+    const data = { email, password }
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/user", {
+        method: "POST", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-            if(response.ok){
-            alert("signup Succesfuly")
-            }
-          } catch (error) {
-            console.error("Error:", error);
-          }
-        }
-  
-    return (
-        <Forms signin={false} onFormSubmit={onSubmit} />
-    )
+
+      if (response.ok) {
+        alert("signup Succesfuly")
+      }
+      if (response.ok) {
+        router.replace('/Forms/login')
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
+
+  return (
+    <Forms signin={false} onFormSubmit={onSubmit} />
+  )
+}
 export default signup
